03a1a5a04aacc00910689ae9fc54357597b7fe81
xiaocai <tycai1@sina.com>
2016-04-04 12:03:53 +0800

    初始化项目

---
+++ UIAlertController/UIAlertController/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  UIAlertController
+//
+//  Created by 小蔡 on 16/4/4.
+//  Copyright © 2016年 xiaocai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setTitle:@"登录" forState:UIControlStateNormal];
+    
+}
+
+
+@end
