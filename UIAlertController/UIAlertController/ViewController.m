2a5496ddd977d7eb7f4ab4aaea32a97130792162
xiaocai <tycai1@sina.com>
2016-04-04 14:06:46 +0800

    AlertController显示

--- UIAlertController/UIAlertController/ViewController.m
+++ UIAlertController/UIAlertController/ViewController.m
@@ -17,7 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
     [button setTitle:@"TEST" forState:UIControlStateNormal];
     [button setBackgroundColor:[UIColor blueColor]];
@@ -35,6 +34,34 @@
 }
 
 - (void)btnClick{
+    /*
+    UIAlertControllerStyleActionSheet: UIActionSheet样式
+    UIAlertControllerStyleAlert: UIAlertView样式
+     */
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"消息" message:@"详细信息" preferredStyle:UIAlertControllerStyleActionSheet];
+    /*
+     UIAlertActionStyleDefault 默认
+     UIAlertActionStyleCancel 取消
+     UIAlertActionStyleDestructive 确认毁灭性的操作
+     */
+    
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"cancel");
+    }];
+    
+    UIAlertAction *destructive = [UIAlertAction actionWithTitle:@"确认" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"destructive");
+    }];
+    
+//    UIAlertAction *defaultAction = [UIAlertAction actionWithTitle:@"默认" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+//        NSLog(@"default");
+//    }];
+    
+    [alert addAction:cancel];
+    [alert addAction:destructive];
+    //[alert addAction:defaultAction];
+    
+    [self presentViewController:alert animated:YES completion:nil];
     
 }
 
